6cd21215a5b5e4257ba794ac364025116f649c66
Bhupendra <bhupendrasharma18@gmail.com>
2020-03-28 18:24:17 +0530

    Added RootController on window

--- NoStoryboard/NoStoryboard/SceneDelegate.swift
+++ NoStoryboard/NoStoryboard/SceneDelegate.swift
@@ -17,7 +17,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        /*
+         Change
+         guard let _ = (scene as? UIWindowScene) else { return }
+         this to
+         guard let windowScene = (scene as? UIWindowScene) else { return }
+         */
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Create and assign your root UIViewController or UINavigationController or UITabBarController to window?.rootViewController
+        let viewController = HomeViewController.init(nibName: "HomeViewController", bundle: nil)
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
